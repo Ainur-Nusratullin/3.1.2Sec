72c0b7b1c907f6bfdba7a9b0fbdf7de3ea20ce4c
Ainur Nusratullin <dr.nusratullin@yandex.ru>
2025-02-12 18:15:13 +0500

    Правки 10

--- 3.1.2Sec/src/main/java/ru/nusratullin/bootcrud/ProjectBoot/model/User.java
+++ 3.1.2Sec/src/main/java/ru/nusratullin/bootcrud/ProjectBoot/model/User.java
@@ -30,7 +30,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST})
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
